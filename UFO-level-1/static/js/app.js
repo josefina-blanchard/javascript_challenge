9573135e5a0a3a046991bc0af6e3e854f22d89f9
Josefina Blanchard <josefina_blanchard@ezcorp.com>
2019-10-30 20:06:24 -0500

    updated files

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,14 +2,18 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-let tbody = d3.select("tbody");
+var tbody = d3.select("tbody");
 
+function buildTable(table){
 data.forEach(d => {
     var row = tbody.append("tr");
     for (key in d){
         row.append("td").text(d[key]);
     }
-});
+  });
+}
+
+buildTable(tableData);
 
 //Referencing the button on the page
 var button = d3.select("#filter-btn");
@@ -20,13 +24,17 @@ var input = d3.select("#datetime");
 button.on("click", function() {
     console.log("test");
     console.log(d3.event.target);
-    var input = d3.select("#datetime");
-    newTable = data.filter(d => data.datetime === input);
-    newTable.forEach(d => {
-        row = tbody.append("tr");
-        for (key in d){
-            row.append("td").text(d[key]);
-        } 
+    var input = d3.select("#datetime").property("value");
+    console.log(input)
+    filterTable = tableData.filter(d => d.datetime === input);
+    console.log(filterTable);
+    tbody.html("");
+    filterTable.forEach(d => {
+       row = tbody.append("tr");
+       for (key in d){
+           row.append("td").text(d[key]);
+       } 
+     buildTable(filterTable);
     })
 });
 
